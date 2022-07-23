c551e92cc9e4fdfc568aa3f9231902a2f24c492b
Arya Jalali <nothatcreative5@gmail.com>
2022-07-23 21:30:47 +0430

    changed the type of contents in the FileReader class from String to StringBuilder.

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,21 +1,22 @@
 import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.HashMap;
 
 public final class FileReader {
     public static String readFile(File file) throws Exception {
-        String contents = "";
+        StringBuilder contents = new StringBuilder();
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
-            contents += scanner.nextLine();
+            contents.append(scanner.nextLine());
         }
         scanner.close();
-        return contents;
+        return contents.toString();
     }
 
     public static HashMap<String, String> readFolder(File folder) throws Exception {
         HashMap<String, String> docIdToContents = new HashMap<>();
-        for (File file : folder.listFiles()) {
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
             docIdToContents.put(file.getName(), readFile(file));
         }
         return docIdToContents;
