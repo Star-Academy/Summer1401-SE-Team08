1a6d6f6fbb46b5f37f0271ebbef54a77b49dec1c
KhosroMoeini <KhosroMoeini2003@gmail.com>
2022-07-25 01:43:49 +0430

    Revert "Co-authored-by: nothatcreative5 <nothatcreative5@users.noreply.github.com>"
    This reverts commit 9964b3c9de2ba3712c8191ba0d4b8c233f37af40.

--- Phase03/Student.cs
+++
@@ -1,9 +0,0 @@
-    namespace StudentSpace;
-
-    using ScoreSpace;
-
-    public class Student {
-    public int StudentNumber {get; init;}
-    public string FirstName {get; init;}
-    public string LastName {get; init;}
-}
