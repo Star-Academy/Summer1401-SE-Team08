d8067fa5886b060cbad666e3242b32c7e8d5cda1
Arya Jalali <nothatcreative5@gmail.com>
2022-07-28 22:34:21 +0430

    increased coverage, and added mocking.

--- Phase04/OperatorTester.cs
+++ Phase04/OperatorTester.cs
@@ -14,7 +14,6 @@ public class CalculatorTester
     private readonly MultiplyOperator _multiplyOperator;
     private readonly DivisionOperator _divisionOperator;
     public CalculatorTester() {
-        //arrange
         this._calculator = new Calculator();
         this._provider = new OperatorProvider();
         this._sumOperator = new SumOperator();
@@ -165,4 +164,58 @@ public class CalculatorTester
         Action act = () => this._divisionOperator.Calculate(firstOperand, secondOperand);
         Assert.Throws<DivideByZeroException>(act);
     }
+
+    [Theory]
+    [InlineData(0,1,1)]
+    [InlineData(2,0,2)]
+    [InlineData(-3,0,-3)]
+    public void CalculatorAdditionTest(int firstOperand, int secondOperand, int expected) {
+        var provider_sub = new Mock<IOperatorProvider>();
+        provider_sub.Setup(x => x.GetOperator(OperatorEnum.sum)).Returns(new SumOperator());
+        Calculator calculator = new Calculator(provider_sub.Object);
+        int actual = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.sum);
+        Assert.Equal(actual, expected);
+    }
+
+    [Theory]
+    [InlineData(0,1,-1)]
+    [InlineData(2,0,2)]
+    [InlineData(-3,0,-3)]
+    public void CalculatorSubTest(int firstOperand, int secondOperand, int expected) {
+        var provider_sub = new Mock<IOperatorProvider>();
+        provider_sub.Setup(x => x.GetOperator(OperatorEnum.sub)).Returns(new SubOperator());
+        Calculator calculator = new Calculator(provider_sub.Object);
+        int actual = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.sub);
+        Assert.Equal(actual, expected);
+    }
+
+
+    [Theory]
+    [InlineData(5,6,30)]
+    [InlineData(6,5,30)]
+    [InlineData(7,-8,-56)]
+    public void CalculatorMultiplicationTest(int firstOperand, int secondOperand, int expected) {
+        var provider_sub = new Mock<IOperatorProvider>();
+        provider_sub.Setup(x => x.GetOperator(OperatorEnum.multiply)).Returns(new MultiplyOperator());
+        Calculator calculator = new Calculator(provider_sub.Object);
+        int actual = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.multiply);
+        Assert.Equal(actual, expected);
+    }
+
+    [Theory]
+    [InlineData(-8,7,-1)]
+    [InlineData(-9,10,0)]
+    [InlineData(10,-9,-1)]
+    public void CalculatorDivisionTest(int firstOperand, int secondOperand, int expected) {
+        var provider_sub = new Mock<IOperatorProvider>();
+        provider_sub.Setup(x => x.GetOperator(OperatorEnum.division)).Returns(new DivisionOperator());
+        Calculator calculator = new Calculator(provider_sub.Object);
+        int actual = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.division);
+        Assert.Equal(actual, expected);
+    }
+
+
+
+
+    
 }
