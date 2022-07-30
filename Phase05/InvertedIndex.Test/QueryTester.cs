fa3ada2576b3f793b0ca1d548834b08b6589e7b3
Arya Jalali <nothatcreative5@gmail.com>
2022-07-31 00:05:13 +0430

    Query Tests added.

---
+++ Phase05/InvertedIndex.Test/QueryTester.cs
@@ -0,0 +1,44 @@
+﻿namespace InvertedIndex.Test;
+
+public class QueryTester
+{
+    private Query _query;
+
+    public QueryTester()
+    {
+        _query = new Query(
+            new List<string>()
+            {
+                "+SALAM",
+                "-KHOB",
+                "HASTID?",
+                "+MAMNON",
+                "+SHOMA",
+                "-CHETORID"
+            }
+            );
+    }
+
+
+    [Fact]
+    public void QueryWordsTest()
+    {
+        Assert.Equal(_query.andWords, new List<string>()
+        {
+            "HASTID?"
+        });
+        
+        Assert.Equal(_query.orWords, new List<string>()
+        {
+            "SALAM",
+            "MAMNON",
+            "SHOMA"
+        });
+        
+        Assert.Equal(_query.notWords, new List<string>()
+        {
+            "KHOB",
+            "CHETORID"
+        });
+    }
+}
