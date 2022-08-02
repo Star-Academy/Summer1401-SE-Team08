7691cf7366abf3320ae4ad6ba5e9df0e63e888da
Arya Jalali <nothatcreative5@gmail.com>
2022-08-02 15:04:06 +0430

    Console program added

---
+++ Phase05-Console/Phase05-Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using InvertedIndex;
+
+namespace Phase05_Console;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = @"C:\Users\Arya\OneDrive\Desktop\are\Summer1401-SE-Team08\Phase05-Console\EnglishData";
+        InvertedIndex.InvertedIndex index = new InvertedIndex.InvertedIndex(path);
+        var result = index.SearchDocsForQuery("God");
+        Console.WriteLine(string.Join(", ", result));
+    }
+}
