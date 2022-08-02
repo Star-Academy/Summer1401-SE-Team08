5843dd75dc686005c1d90a1c43b339d7ed67b63f
Arya Jalali <nothatcreative5@gmail.com>
2022-08-02 20:57:15 +0430

    100 percent code coverage

---
+++ Phase05/InvertedIndex.Test/IndexTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using InvertedIndex.Abstraction;
+
+namespace InvertedIndex.Test;
+
+public class InvertedIndexTest
+{
+    private Index Index;
+
+    [Fact]
+    public void HandleQuery_Should_ProduceResult()
+    {
+        var builder = new Mock<IQueryBuilder>();
+        builder.Setup(x => x.BuildQuery(new List<string>()
+        {
+            "ARYA",
+            "KHOSRO"
+        })).Returns(new Query());
+        var tokenizer = new Mock<ITokenizer>();
+        tokenizer.Setup(x => x.Tokenize("test")).Returns(new List<string>()
+        {
+            "ARYA",
+            "KHOSRO"
+        });
+        var handler = new Mock<IHandler>();
+        handler.Setup(x => x.HandleQuery(new Query())).Returns(new HashSet<string>()
+        {
+            "12", "13", "15"
+        });
+
+        Index = new Index(null);
+        Index.QueryBuilder = builder.Object;
+        Index.Tokenizer = tokenizer.Object;
+        Index.QueryHandler = handler.Object;
+
+        var actual = Index.HandleQuery("test");
+        var expected = new HashSet<string>()
+        {
+            "12", "13", "15"
+        };
+        actual.Should().Equal(expected);
+    }
+}
---
+++ Phase05/InvertedIndex/Index.cs
@@ -0,0 +1,26 @@
+﻿using InvertedIndex.Abstraction;
+using InvertedIndex.Enums;
+
+namespace InvertedIndex;
+public class Index
+{
+
+    public IQueryBuilder QueryBuilder { get; set; }
+    public IHandler QueryHandler { get; set; }
+    public ITokenizer Tokenizer { get; set; }
+
+    public Index(SearchEngine engine)
+    {
+        QueryBuilder = new QueryBuilder();
+        Tokenizer = new Tokenizer(TokenizerMode.Text);
+        QueryHandler = new QueryHandler(engine);
+    }
+
+    public HashSet<string> HandleQuery(string query)
+    {
+        var @out = QueryHandler.HandleQuery
+            (QueryBuilder.BuildQuery(Tokenizer.Tokenize(query)));
+        return @out;
+    }
+    
+}
