7747c0260a7a2989b21285cf3cdf86e47df11ee1
KhosroMoeini <KhosroMoeini2003@gmail.com>
2022-08-02 15:12:16 +0430

    Update FileReaderTest.cs

--- Phase05/InvertedIndex.Test/FileReaderTest.cs
+++ Phase05/InvertedIndex.Test/FileReaderTest.cs
@@ -1,26 +1,26 @@
-﻿// using InvertedIndex.Abstraction;
-//
-// namespace InvertedIndex.Test;
-//
-// public class FileReaderTest
-// {
-//     private readonly IFileReader _fileReader;
-//
-//     public FileReaderTest()
-//     {
-//         _fileReader = new FileReader();
-//     }
-//     [Fact]
-//     public void Read_Folder_Result()
-//     {
-//         var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName +
-//                    "\\ReadFolderTest";
-//         var actual = _fileReader.ReadFolder(path);
-//         var expected = new Dictionary<string, string>()
-//         {
-//             ["1.txt"] = "a",
-//             ["2.txt"] = "b"
-//         };
-//         Assert.Equal(expected,actual);
-//     }
-// }
+﻿using InvertedIndex.Abstraction;
+
+namespace InvertedIndex.Test;
+
+public class FileReaderTest
+{
+    private readonly IFileReader _fileReader;
+    public FileReaderTest()
+    {
+        _fileReader = new FileReader();
+    }
+    
+    [Fact]
+    public void Read_Folder_Result()
+    {
+        var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName +
+                        "\\ReadFolderTest";
+        var actual = _fileReader.ReadFolder(path);
+        var expected = new Dictionary<string, string>()
+        {
+            ["1.txt"] = "a",
+            ["2.txt"] = "b"
+        };
+        Assert.Equal(expected,actual);
+    }
+}
