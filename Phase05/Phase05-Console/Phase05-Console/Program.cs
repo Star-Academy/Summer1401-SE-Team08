0ffb8f7bb5a1d9caf8ef905c59c85f79ba51fb68
Arya Jalali <nothatcreative5@gmail.com>
2022-08-12 00:46:53 +0430

    Done.

--- Phase05/Phase05-Console/Phase05-Console/Program.cs
+++ Phase05/Phase05-Console/Phase05-Console/Program.cs
@@ -4,10 +4,10 @@ namespace Phase05_Console;
 
 public class Program
 {
+    private const string Path = @"C:\Users\Arya\OneDrive\Desktop\are\Summer1401-SE-Team08\Phase05-Console\EnglishData";
     public static void Main(string[] args)
     {
-        var path = @"C:\Users\Arya\OneDrive\Desktop\are\Summer1401-SE-Team08\Phase05-Console\EnglishData";
-        var index = new InvertedIndex.InvertedIndex(path);
+        var index = new InvertedIndex.InvertedIndex(Path);
         var query = Console.ReadLine();
         var result = index.SearchDocsForQuery(query);
         Console.WriteLine(string.Join(", ", result));
