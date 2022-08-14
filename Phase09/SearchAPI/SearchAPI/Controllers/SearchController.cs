4a79d4fca30a2371977b6d103ea4ab77a81ff387
Arya Jalali <nothatcreative5@gmail.com>
2022-08-14 15:29:03 +0430

    First draft

---
+++ Phase09/SearchAPI/SearchAPI/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class SearchController
+{
+    private const string Path = @"C:\Users\Arya\OneDrive\Desktop\EnglishData";
+    private static readonly InvertedIndex.InvertedIndex SearchEngine = new(Path);
+
+    [HttpGet]
+    public HashSet<string>? Search([FromQuery(Name = "Query")] string query)
+    {
+        return SearchEngine.SearchDocsForQuery(query);
+    }
+}
